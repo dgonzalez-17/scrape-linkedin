257396c08d7e811591a98c67062a78884bc248cd
Juan Campuzano <juanfecazuluaga@gmail.com>
2023-12-28 16:45:50 -0500

    Print inner html user

--- main.py
+++ main.py
@@ -246,6 +246,7 @@ def scrape_company(request: ScrapeCompany):
 def scrape_user(request: ScrapeUser):
     url = request.url
     driver.get(url)
+    print(driver.get_attribute('innerHTML'))
     try:
         time.sleep(1)
         driver.find_element(By.CSS_SELECTOR, '#public_profile_contextual-sign-in > div > section > button').click()
