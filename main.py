29a68946b7f7fdda985d6243306601d4c765abb3
Juan Campuzano <juanfecazuluaga@gmail.com>
2024-01-14 21:20:12 -0500

    Fox options

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ PASSWORD = 'WebscrapingLinkedin50'
 options = webdriver.ChromeOptions()
 options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 options.add_argument("start-maximized")
-ptions.add_argument("--disable-blink-features=AutomationControlled")
+options.add_argument("--disable-blink-features=AutomationControlled")
 options.add_experimental_option("excludeSwitches", ["enable-automation"])
 options.add_argument("--headless")
 options.add_argument("--no-sandbox")
